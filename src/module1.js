2bc55fd53dce268c12550837a1b450be9ab1eb6f
cjshinning <784319417@qq.com>
2018-05-19 15:31:56 +0800

    ES6

---
+++ src/module1.js
@@ -0,0 +1,9 @@
+export const name = 'imooc'
+
+export function sayHello(){
+    console.log('imooc rocks!')
+}
+
+export default function test(){
+    console.log('hello imooc')
+}
---
+++ src/module2.js
@@ -0,0 +1,6 @@
+// import {name, sayHello} from './module1'
+// console.log(name)
+// sayHello()
+
+import * as mod1 from './module1'
+console.log(mod1)
