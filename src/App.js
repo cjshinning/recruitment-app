50284889736dc9b5c1c85e955d43d6c7e68af1b5
cjshinning <784319417@qq.com>
2018-05-20 09:44:14 +0800

    redux和react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {addGUN} from './index.redux'
+
+class App extends React.Component{
+
+    render(){
+        const store = this.props.store
+        const num = store.getState()
+        return (
+            <div>
+                <h1>现在有机枪{num}把</h1>
+                <button onClick={()=>store.dispatch(addGUN())}>申请武器</button>
+            </div>
+        )
+    }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,30 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 import {createStore} from 'redux'
+import App from './App'
+import {counter} from './index.redux'
 
-// 创建reducer
-function counter(state=0, action){
-    switch(action.type){
-        case 'ADD_GUN':
-            return state + 1
-        case 'REMOVE_GUN':
-            return state - 1
-        default:
-            return 10
-    }
+const store = createStore(counter)
+
+function render(){
+    ReactDOM.render(<App store={store}/>, document.getElementById('root'))
 }
 
-// 1.新建store
-let store = createStore(counter)
+render()
 
-const init = store.getState(store)
-console.log(init)
-
-function listner(){
-    const current = store.getState()
-    console.log(`现在的机枪有${current}把`)
-}
-
-store.subscribe(listner)
-
-store.dispatch({type: 'ADD_GUN'})
-store.dispatch({type: 'ADD_GUN'})
-store.dispatch({type: 'REMOVE_GUN'})
+store.subscribe(render)
