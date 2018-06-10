fb02590053e5bb0344bded5cf4ea4d1856b4d805
cjshinning <784319417@qq.com>
2018-06-10 11:32:37 +0800

    高级组件demo

--- src/container/login/login.js
+++ src/container/login/login.js
@@ -4,22 +4,52 @@ import { List, InputItem, WhiteSpace, WingBlank, Button } from 'antd-mobile'
 import {connect} from 'react-redux'
 import {login} from '../../redux/user.redux'
 import {Redirect} from 'react-router-dom'
+import { wrap } from 'module';
 
-function hello(){
-    console.log('hello imooc I love react')
+// function hello(){
+//     console.log('hello imooc I love react')
+// }
+
+// function WrapperHello(fn){
+//     return function(){
+//         console.log('before say hello')
+//         fn()
+//         console.log('after say hello')
+//     }
+// }
+
+// hello = WrapperHello(hello)
+
+// hello()
+
+
+function WrapperHello(Comp){
+    class WrapComp extends React.Component{
+        render(){
+            return(
+                <div>
+                    <p>这是HOC高阶组件特有的元素</p>
+                    <Comp {...this.props}></Comp>
+                </div>
+            )
+        }
+    }
+    return WrapComp
 }
 
-function WrapperHello(fn){
-    return function(){
-        console.log('before say hello')
-        fn()
-        console.log('after say hello')
+
+
+
+@WrapperHello
+class Hello extends React.Component{
+    render(){
+        return <h2>hello imooc I love React&Redux</h2>
     }
 }
 
-hello = WrapperHello(hello)
 
-hello()
+
+Hello = WrapperHello(Hello)
 
 @connect(
     state=>state.user,
